f0aa3dc2c4406fa63eace49e27eef17002550750
Denis <den24.03@mail.ru>
2023-06-30 00:31:55 +0500

    Added site

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Book-Shop
+https://www.figma.com/file/fa75CDeoGoAnZqFTJDrsMT/Книги-HTML-Academy-(Copy)?node-id=0-1&t=8lzgsZkNhM8FR6EH-0
---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+const moreBtn1 = document.querySelectorAll('.page-content-new-book-btn');
+console.log(moreBtn1);
+moreBtn1.forEach(elem => {
+    elem.classList.add('page-content-book-btn');
+});
+const moreBtn2 = document.querySelectorAll('.page-content-best-book-btn');
+console.log(moreBtn2);
+moreBtn2.forEach(elem => {
+    elem.classList.add('page-content-book-btn');
+});
+
+
+
+
+

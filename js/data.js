9c1fc26eb3adb6bdda05a1d3577ecd11ff85586a
Denis <den24.03@mail.ru>
2023-07-07 18:19:57 +0500

    Доработал catalog.html

---
+++ js/data.js
@@ -0,0 +1,137 @@
+export const newBooks = `[
+    {
+      "reatingCount": 3.5,
+      "image": "img/new-book1.jpg",
+      "author": "Палиенко&nbsp;А.",
+      "name": "Жизнь в&nbsp;удовольствие",
+      "discription": "Автор собрал в&nbsp;этой книге все свои знания, актуальные в&nbsp;современном мире",
+      "countPage": "208 страниц"
+    },
+    {
+      "reatingCount": 4.8,
+      "image": "img/new-book2.jpg",
+      "author": "Несбё&nbsp;Ю.",
+      "name": "Королевство",
+      "discription": "В&nbsp;норвежском городке, затерянном в&nbsp;горах, течет сонная, мирная жизнь",
+      "countPage": "576 страниц"
+    },
+    {
+      "reatingCount": 3.5,
+      "image": "img/new-book3.jpg",
+      "author": "Пелевин&nbsp;В.",
+      "name": "Transhumanism inc.",
+      "discription": "В&nbsp;будущем богатые люди смогут отделить свой мозг от&nbsp;старящегося тела",
+      "countPage": "608 страниц"
+    },
+    {
+      "reatingCount": 4.1,
+      "image": "img/new-book4.jpg",
+      "author": "Санд&nbsp;И.",
+      "name": "Чувство стыда",
+      "discription": "Это чувство сидит глубоко в&nbsp;нас, о&nbsp;нем не&nbsp;принято распространяться и&nbsp;обсуждать с&nbsp;близкими",
+      "countPage": "170 страниц"
+    }
+
+]`;
+
+export const bestBooks = `[
+  {
+    "reatingCount": 5.0,
+    "image": "img/best-book1.jpg",
+    "author": "Бакман&nbsp;Ф.",
+    "name": "Тревожные люди",
+    "discription": "Целая галерея уязвимых персонажей, тонко подмеченная Бакманом человеческая природа",
+    "countPage": "416 страниц"
+  },
+  {
+    "reatingCount": 4.9,
+    "image": "img/best-book2.jpg",
+    "author": "Харари&nbsp;Ю.",
+    "name": "Sapiens",
+    "discription": "Краткая история человечества. Какое будущее нас ожидает? Кто наши предки?",
+    "countPage": "520 страниц"
+  },
+  {
+    "reatingCount": 4.8,
+    "image": "img/best-book3.jpg",
+    "author": "Кинг&nbsp;С.",
+    "name": "Институт",
+    "discription": "Бежать невозможно. Будущее предопределено. Загадочная Дальняя половина Института",
+    "countPage": "608 страниц"
+  },
+  {
+    "reatingCount": 4.7,
+    "image": "img/best-book4.jpg",
+    "author": "Руни&nbsp;С.",
+    "name": "Нормальные люди",
+    "discription": "О&nbsp;желании ощутить власть над другим человеком и&nbsp;одновременно всецело принадлежать ем",
+    "countPage": "288 страниц"
+  }
+]`;
+
+export const catalogBooks = `[
+  {
+    "reatingCount": 3.8,
+    "image": "img/catalog-img/book1.jpg",
+    "author": "Кагава&nbsp;Д.",
+    "name": "Железный король",
+    "discription": "Меган удалось победить Железного короля, и&nbsp;теперь она во&nbsp;Дворце",
+    "countPage": "416 страниц"
+  },
+  {
+    "reatingCount": 4.2,
+    "image": "img/catalog-img/book2.jpg",
+    "author": "Сакавич&nbsp;Н.",
+    "name": "Лисья нора",
+    "discription": "Команда Лисов&mdash;игроки экси, которые пытаются выиграть чемпионат мира",
+    "countPage": "336 страниц"
+  },
+  {
+    "reatingCount": 4.5,
+    "image": "img/catalog-img/book3.jpg",
+    "author": "Акунин&nbsp;Б.",
+    "name": "Просто маса",
+    "discription": "Масу ожидают невероятные потрясения и&nbsp;невероятные приключения",
+    "countPage": "448 страниц"
+  },
+  {
+    "reatingCount": 3.9,
+    "image": "img/catalog-img/book4.jpg",
+    "author": "Макконахи&nbsp;М.",
+    "name": "Зеленый свет",
+    "discription": "Как быть честным. Как избежать стресса. Как радоваться жизни",
+    "countPage": "320 страниц"
+  },
+  {
+    "reatingCount": 3.5,
+    "image": "img/catalog-img/book5.jpg",
+    "author": "Майер&nbsp;С.",
+    "name": "Солнце полуночи",
+    "discription": "История любви и&nbsp;приключений от&nbsp;лица Эдварда",
+    "countPage": "784 страниц"
+  },
+  {
+    "reatingCount": 4.4,
+    "image": "img/catalog-img/book6.jpg",
+    "author": "Бакман&nbsp;Ф.",
+    "name": "Вторая жизнь Уве",
+    "discription": "Откуда берутся зануды и&nbsp;склочники? Внутри зреет обида на&nbsp;мир",
+    "countPage": "384 страниц"
+  },
+  {
+    "reatingCount": 3.9,
+    "image": "img/catalog-img/book7.jpg",
+    "author": "Быков&nbsp;Д.",
+    "name": "Истребитель",
+    "discription": "Роман о&nbsp;советских летчиках, &laquo;соколах Сталина&raquo;",
+    "countPage": "576 страниц"
+  },
+  {
+    "reatingCount": 4.1,
+    "image": "img/catalog-img/book8.jpg",
+    "author": "Водолазкин&nbsp;Е.",
+    "name": "Лавр",
+    "discription": "Возможно&nbsp;ли любовью и&nbsp;жертвой спасти душу человека?",
+    "countPage": "448 страниц"
+  }
+]`;
--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-
-const moreBtn1 = document.querySelectorAll('.page-content-new-book-btn');
-console.log(moreBtn1);
-moreBtn1.forEach(elem => {
-    elem.classList.add('page-content-book-btn');
+const moreBtn1 = document.querySelectorAll(".page-content-new-book-btn");
+moreBtn1.forEach((elem) => {
+  elem.classList.add("page-content-book-btn");
 });
-const moreBtn2 = document.querySelectorAll('.page-content-best-book-btn');
-console.log(moreBtn2);
-moreBtn2.forEach(elem => {
-    elem.classList.add('page-content-book-btn');
+const moreBtn2 = document.querySelectorAll(".page-content-best-book-btn");
+moreBtn2.forEach((elem) => {
+  elem.classList.add("page-content-book-btn");
 });
-
-
-
-
-
